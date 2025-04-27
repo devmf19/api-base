d8447dfbf94de4ec414305ca206db0a5887fb521
Francisco Martinez <franma.martinez19@gmail.com>
2025-04-26 19:45:24 -0500

    fix minimal bugs

--- src/main/java/com/devmf/base/security/infrastructure/adapter/persistence/service/UserDetailsServiceImpl.java
+++ src/main/java/com/devmf/base/security/infrastructure/adapter/persistence/service/UserDetailsServiceImpl.java
@@ -4,8 +4,6 @@ import com.devmf.base.security.infrastructure.adapter.persistence.repository.Aut
 import com.devmf.base.security.infrastructure.config.model.PrincipalUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,17 +13,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
-
-    private final String NO_ENCODING = "{noop}";
-    private final String ADMIN1 = "ADMIN1";
-
     private final AuthenticationUserEntityRepository authenticationUserEntityRepository;
-
-    @Value("${api.security.default-user}")
-    private String userName;
-
-    @Value("${api.security.default-password}")
-    private String password;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
--- src/main/java/com/devmf/base/user/infrastructure/adapter/api/controller/UserController.java
+++ src/main/java/com/devmf/base/user/infrastructure/adapter/api/controller/UserController.java
@@ -7,8 +7,6 @@ import com.devmf.base.user.application.port.in.UserManager;
 import com.devmf.base.user.infrastructure.adapter.api.dto.request.UserReq;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
